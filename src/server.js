41bc8bc69226f51b52859e38525126b77ad95f39
zzyoung <114982649+zzyoung1018@users.noreply.github.com>
2026-06-11 15:21:26 +0800

    fix: exit cleanly on service shutdown

--- src/server.js
+++ src/server.js
@@ -8,13 +8,30 @@ server.listen(config.port, config.host, () => {
   console.log(`DM Online listening on http://${config.host}:${config.port}`);
 });
 
+let isShuttingDown = false;
+let isDatabaseClosed = false;
+
+function closeDatabase() {
+  if (isDatabaseClosed) return;
+  isDatabaseClosed = true;
+  database.close();
+}
+
 function shutdown(signal) {
+  if (isShuttingDown) return;
+  isShuttingDown = true;
   console.log(`Received ${signal}, shutting down`);
   server.close(() => {
-    database.close();
+    closeDatabase();
     process.exit(0);
   });
-  setTimeout(() => process.exit(1), 10_000).unref();
+
+  server.closeIdleConnections?.();
+  setTimeout(() => {
+    server.closeAllConnections?.();
+    closeDatabase();
+    process.exit(0);
+  }, 10_000).unref();
 }
 
 process.on('SIGINT', shutdown);
